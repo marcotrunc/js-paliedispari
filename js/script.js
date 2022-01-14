4adc0077acc473e3b5dd68aa02bb0e0828c14453
marcotrunc <marcotrunc@gmail.com>
2022-01-14 15:05:55 +0100

    added exercise 1 with bonus

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
-console.log('JS OK')
+//  ! Esercizio 1
 /*Palidroma
     Chiedere all’utente di inserire una parola
     Creare una funzione per capire se la parola inserita è palindroma*/
     
-    // Chiedo una parola all'utente
-    const userWord = prompt('Inserisci una parola', 'anna');
-    
-    // Funzione che controlla la parola inserita
+// Recupero elementi in pagina
+const userWordField = document.getElementById('user-word');
+const buttonWord = document.getElementById('button-word');
+
+const answerContainer = document.getElementById('answer-container');
+const answer = document.getElementById('answer');
+// Recuper Elementi al click
+
+buttonWord.addEventListener('click',function (){
+    userWord = (userWordField.value).trim();
+    console.log(userWord);
+    if(userWord === getinvertedWord(userWord)){
+        answer.innerHTML = "La parola è palindroma";
+        answerContainer.classList.remove('d-none');
+    } else{
+        answer.innerHTML = "La parola non è palindroma";
+        answerContainer.classList.remove('d-none');
+    };
+});
+
+
+
+// Funzione che controlla la parola inserita
     function getinvertedWord(word){
         // Dichiarazione della variabile di concatenazione
         let invertedWord = '';
@@ -14,11 +33,14 @@ console.log('JS OK')
             invertedWord += word[i];
         }
         return invertedWord;
-    }
-console.log(`La parola invertita è: ${getinvertedWord(userWord)}`);
+    };
 
-if(getinvertedWord(userWord) === userWord){
-    alert('è palindorma');
-} else{
-    alert('non è palindorma')
-};
+// ! Esercizio 2
+ /*Pari e Dispari
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer
+    (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto. */
+
